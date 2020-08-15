752c72e7b9a37d9cd8f4f2a0ac08a8d75329894f
ChenyangZhang-cs <chenyangzhang@ruc.edu.cn>
2020-08-15 20:28:47 +0800

    update winograd

--- Winograd/Winograd_convolution.c
+++ Winograd/Winograd_convolution.c
@@ -3,7 +3,21 @@
 #include <omp.h>
 #include <math.h>
 #include <time.h>
+#include<sys/time.h>
 
+
+double rtclock()
+{
+    struct timezone Tzp;
+    struct timeval Tp;
+    int stat;
+    stat = gettimeofday (&Tp, &Tzp);
+    if (stat != 0) printf("Error return from gettimeofday: %d",stat);
+    return(Tp.tv_sec + Tp.tv_usec*1.0e-6);
+}
+
+
+// F(2x2,3x3)
 
 void winograd_GgGt_2x2(float *input, float *output, int K, int C) {
     int total_filter = K * C;
@@ -21,6 +35,13 @@ void winograd_GgGt_2x2(float *input, float *output, int K, int C) {
                 tile[i][j] = input[in_k_stride * k + in_c_stride * c + 3 * i + j];
             }
         }
+
+        // const float G[4][3] = {
+        //     {1.0f, 0.0f, 0.0f},
+        //     {0.5f, 0.5f, 0.5f},
+        //     {0.5f, -0.5f, 0.5f},
+        //     {0.0f, 0.0f, 1.0f}
+        // };
 
         // G * g
         for (int j = 0; j < 3; j ++) {
@@ -75,6 +96,13 @@ int batch_size, int C, int tile_n, int map_size) {
                 tile[i][j] = input[n * in_n_stride + c * in_c_stride + x * x_stride + y * y_stride];
             }
         }
+
+        // const float Bt[4][4] = {
+        //     {1.0f, 0.0f, -1.0f, 0.0f},
+        //     {0.0f, 1.0f, 1.0f, 0.0f},
+        //     {0.0f, -1.0f, 1.0f, 0.0f},
+        //     {0.0f, 1.0f, 0.0f, -1.0f}
+        // }
 
         // Bt * d
         for (int j = 0; j < 4; j ++) {
@@ -131,6 +159,13 @@ int batch_size, int C, int tile_n, int map_size) {
             }
         }
 
+        // const float Bt[4][4] = {
+        //     {1.0f, 0.0f, -1.0f, 0.0f},
+        //     {0.0f, 1.0f, 1.0f, 0.0f},
+        //     {0.0f, -1.0f, 1.0f, 0.0f},
+        //     {0.0f, 1.0f, 0.0f, -1.0f}
+        // }
+
         // Bt * d
         for (int j = 0; j < 4; j ++) {
             t_tile[0][j] = tile[0][j] - tile[2][j];
@@ -183,6 +218,11 @@ int batch_size, int K, int tile_n, int out_map_size, int C) {
                 }
             }   
         }
+
+        // const float At[2][4] {
+        //     {1.0f, 1.0f, 1.0f, 0.0f},
+        //     {0.0f, 1.0f, -1.0f, -1.0f}
+        // }
 
         float t_tile[2][4], f_tile[2][2];
         // At * I
@@ -254,6 +294,9 @@ int batch_size, int C, int K, int map_size, int padding) {
 }
 
 
+
+// F(4x4,3x3)
+
 void winograd_GgGt_4x4(float *input, float *output, int K, int C) {
     int total_filter = K * C;
     int in_c_stride = 9, in_k_stride = in_c_stride * C;
@@ -271,22 +314,40 @@ void winograd_GgGt_4x4(float *input, float *output, int K, int C) {
             }
         }
 
+        // const float G[6][3] = {
+        //     {0.25f, 0.0f, 0.0f},
+
+        //     {-1.0f/6, -1.0f/6, -1.0f/6},
+        //     {-1.0f/6, 1.0f/6, -1.0f/6},
+
+        //     {1.0f/24, 1.0f/12, 1.0f/6},
+        //     {1.0f/24, -1.0f/12, 1.0f/6},
+
+        //     {0.0f, 0.0f, 1.0f}
+        // }
+
         // G * g
         for (int j = 0; j < 3; j ++) {
             t_tile[0][j] = 0.25f * tile[0][j];
+
             t_tile[1][j] = -1.0f/6 * tile[0][j] - 1.0f/6 * tile[1][j] - 1.0f/6 * tile[2][j];
             t_tile[2][j] = -1.0f/6 * tile[0][j] + 1.0f/6 * tile[1][j] - 1.0f/6 * tile[2][j];
+
             t_tile[3][j] = 1.0f/24 * tile[0][j] + 1.0f/12 * tile[1][j] + 1.0f/6 * tile[2][j];
             t_tile[4][j] = 1.0f/24 * tile[0][j] - 1.0f/12 * tile[1][j] + 1.0f/6 * tile[2][j];
+
             t_tile[5][j] = tile[2][j];
         }
         // g * Gt
         for (int i = 0; i < 6; i ++) {
             f_tile[i][0] = 0.25f * t_tile[i][0];
+
             f_tile[i][1] = -1.0f/6 * t_tile[i][0] - 1.0f/6 * t_tile[i][1] - 1.0f/6 * t_tile[i][2];
             f_tile[i][2] = -1.0f/6 * t_tile[i][0] + 1.0f/6 * t_tile[i][1] - 1.0f/6 * t_tile[i][2];
+
             f_tile[i][3] = 1.0f/24 * t_tile[i][0] + 1.0f/12 * t_tile[i][1] + 1.0f/6 * t_tile[i][2];
             f_tile[i][4] = 1.0f/24 * t_tile[i][0] - 1.0f/12 * t_tile[i][1] + 1.0f/6 * t_tile[i][2];
+
             f_tile[i][5] = t_tile[i][2];
         }
 
@@ -329,22 +390,40 @@ int batch_size, int C, int tile_n, int map_size) {
             }
         }
 
+        // const float Bt[6][6] = {
+        //     {4.0f, 0.0f, -5.0f, 0.0f, 1.0f, 0.0f},
+
+        //     {0.0f, -4.0f, -4.0f, 1.0f, 1.0f, 0.0f},
+        //     {0.0f, 4.0f, -4.0f, -1.0f, 1.0f, 0.0f},
+
+        //     {0.0f, -2.0f, -1.0f, 2.0f, 1.0f, 0.0f},
+        //     {0.0f, 2.0f, -1.0f, -2.0f, 1.0f, 0.0f},
+
+        //     {0.0f, 4.0f, 0.0f, -5.0f, 0.0f, 1.0f}
+        // }
+
         // Bt * d
         for (int j = 0; j < 6; j ++) {
             t_tile[0][j] = 4.0f * tile[0][j] - 5.0f * tile[2][j] + tile[4][j];
+
             t_tile[1][j] = -4.0f * tile[1][j] - 4.0f * tile[2][j] + tile[3][j] + tile[4][j];
             t_tile[2][j] = 4.0f * tile[1][j] - 4.0f * tile[2][j] - tile[3][j] + tile[4][j];
+
             t_tile[3][j] = -2.0f * tile[1][j] - tile[2][j] + 2.0f * tile[3][j] + tile[4][j];
             t_tile[4][j] = 2.0f * tile[1][j] - tile[2][j] - 2.0f * tile[3][j] + tile[4][j];
+
             t_tile[5][j] = 4.0f * tile[1][j] - 5.0f * tile[3][j] + tile[5][j];
         }
         // d * B
         for (int i = 0; i < 6; i ++) {
             tile[i][0] = 4.0f * t_tile[i][0] - 5.0f * t_tile[i][2] + t_tile[i][4];
+
             tile[i][1] = -4.0f * t_tile[i][1] - 4.0f * t_tile[i][2] + t_tile[i][3] + t_tile[i][4];
             tile[i][2] = 4.0f * t_tile[i][1] - 4.0f * t_tile[i][2] - t_tile[i][3] + t_tile[i][4];
+
             tile[i][3] = -2.0f * t_tile[i][1] - t_tile[i][2] + 2.0f * t_tile[i][3] + t_tile[i][4];
             tile[i][4] = 2.0f * t_tile[i][1] - t_tile[i][2] - 2.0f * t_tile[i][3] + t_tile[i][4];
+
             tile[i][5] = 4.0f * t_tile[i][1] - 5.0f * t_tile[i][3] + t_tile[i][5];
         }
 
@@ -387,6 +466,18 @@ int batch_size, int C, int tile_n, int map_size) {
                 }
             }
         }
+
+        // const float Bt[6][6] = {
+        //     {4.0f, 0.0f, -5.0f, 0.0f, 1.0f, 0.0f},
+
+        //     {0.0f, -4.0f, -4.0f, 1.0f, 1.0f, 0.0f},
+        //     {0.0f, 4.0f, -4.0f, -1.0f, 1.0f, 0.0f},
+
+        //     {0.0f, -2.0f, -1.0f, 2.0f, 1.0f, 0.0f},
+        //     {0.0f, 2.0f, -1.0f, -2.0f, 1.0f, 0.0f},
+
+        //     {0.0f, 4.0f, 0.0f, -5.0f, 0.0f, 1.0f}
+        // }
 
         // Bt * d
         for (int j = 0; j < 6; j ++) {
@@ -444,6 +535,13 @@ int batch_size, int K, int tile_n, int out_map_size, int C) {
                 }
             }   
         }
+
+        // const float At[4][6] {
+        //     {1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f},
+        //     {0.0f, 1.0f, -1.0f, 2.0f, -2.0f, 0.0f},
+        //     {0.0f, 1.0f, 1.0f, 4.0f, 4.0f, 0.0f},
+        //     {0.0f, 1.0f, -1.0f, 8.0f, -8.0f, 1.0f}
+        // }
 
         float t_tile[4][6], f_tile[4][4];
         // At * I
@@ -519,6 +617,9 @@ int batch_size, int C, int K, int map_size, int padding) {
 }
 
 
+
+// F(6x6,3x3)
+
 void winograd_GgGt_6x6(float *input, float *output, int K, int C) {
     int total_filter = K * C;
     int in_c_stride = 9, in_k_stride = in_c_stride * C;
@@ -536,26 +637,49 @@ void winograd_GgGt_6x6(float *input, float *output, int K, int C) {
             }
         }
 
+        // const float G[8][3] = { 
+        //     {   1.0f,     0.0f,     0.0f},
+
+        //     {-2.0f/9,  -2.0f/9,  -2.0f/9},
+        //     {-2.0f/9,   2.0f/9,  -2.0f/9},
+
+        //     {1.0f/90,  1.0f/45,  2.0f/45},
+        //     {1.0f/90, -1.0f/45,  2.0f/45},
+
+        //     {1.0f/45,  1.0f/90, 1.0f/180},
+        //     {1.0f/45, -1.0f/90, 1.0f/180},
+
+        //     {   0.0f,     0.0f,     1.0f}
+        // };
+
         // G * g
         for (int j = 0; j < 3; j ++) {
             t_tile[0][j] = tile[0][j];
+
             t_tile[1][j] = -2.0f/9 * tile[0][j] - 2.0f/9 * tile[1][j] - 2.0f/9 * tile[2][j];
             t_tile[2][j] = -2.0f/9 * tile[0][j] + 2.0f/9 * tile[1][j] - 2.0f/9 * tile[2][j];
+
             t_tile[3][j] = 1.0f/90 * tile[0][j] + 1.0f/45 * tile[1][j] + 2.0f/45 * tile[2][j];
             t_tile[4][j] = 1.0f/90 * tile[0][j] - 1.0f/45 * tile[1][j] + 2.0f/45 * tile[2][j];
+
             t_tile[5][j] = 1.0f/45 * tile[0][j] + 1.0f/90 * tile[1][j] + 1.0f/180 * tile[2][j];
             t_tile[6][j] = 1.0f/45 * tile[0][j] - 1.0f/90 * tile[1][j] + 1.0f/180 * tile[2][j];
+
             t_tile[7][j] = tile[2][j];
         }
         // g * Gt
         for (int i = 0; i < 8; i ++) {
             f_tile[i][0] = t_tile[i][0];
+
             f_tile[i][1] = -2.0f/9 * t_tile[i][0] - 2.0f/9 * t_tile[i][1] - 2.0f/9 * t_tile[i][2];
             f_tile[i][2] = -2.0f/9 * t_tile[i][0] + 2.0f/9 * t_tile[i][1] - 2.0f/9 * t_tile[i][2];
+
             f_tile[i][3] = 1.0f/90 * t_tile[i][0] + 1.0f/45 * t_tile[i][1] + 2.0f/45 * t_tile[i][2];
             f_tile[i][4] = 1.0f/90 * t_tile[i][0] - 1.0f/45 * t_tile[i][1] + 2.0f/45 * t_tile[i][2];
+
             f_tile[i][5] = 1.0f/45 * t_tile[i][0] + 1.0f/90 * t_tile[i][1] + 1.0f/180 * t_tile[i][2];
             f_tile[i][6] = 1.0f/45 * t_tile[i][0] - 1.0f/90 * t_tile[i][1] + 1.0f/180 * t_tile[i][2];
+
             f_tile[i][7] = t_tile[i][2];
         }
 
@@ -597,6 +721,21 @@ int batch_size, int C, int tile_n, int map_size) {
                 tile[i][j] = input[n * in_n_stride + c * in_c_stride + x * x_stride + y * y_stride];
             }
         }
+
+        // const float Bt[8][8] = {
+        //     {1.0f,  0.0f, -5.25f,  0.00f,  5.25f,  0.00f, -1.0f, 0.0f},
+
+        //     {0.0f,  1.0f,  1.00f, -4.25f, -4.25f,  1.00f,  1.0f, 0.0f},
+        //     {0.0f, -1.0f,  1.00f,  4.25f, -4.25f, -1.00f,  1.0f, 0.0f},
+
+        //     {0.0f,  0.5f,  0.25f, -2.50f, -1.25f,  2.00f,  1.0f, 0.0f},
+        //     {0.0f, -0.5f,  0.25f,  2.50f, -1.25f, -2.00f,  1.0f, 0.0f},
+
+        //     {0.0f,  2.0f,  4.00f, -2.50f, -5.00f,  0.50f,  1.0f, 0.0f},
+        //     {0.0f, -2.0f,  4.00f,  2.50f, -5.00f, -0.50f,  1.0f, 0.0f},
+
+        //     {0.0f, -1.0f,  0.00f,  5.25f,  0.00f, -5.25f,  0.0f, 1.0f}
+        // };
 
         // Bt * d
         for (int j = 0; j < 8; j ++) {
@@ -669,6 +808,21 @@ int batch_size, int C, int tile_n, int map_size) {
             }
         }
 
+        // const float Bt[8][8] = {
+        //     {1.0f,  0.0f, -5.25f,  0.00f,  5.25f,  0.00f, -1.0f, 0.0f},
+
+        //     {0.0f,  1.0f,  1.00f, -4.25f, -4.25f,  1.00f,  1.0f, 0.0f},
+        //     {0.0f, -1.0f,  1.00f,  4.25f, -4.25f, -1.00f,  1.0f, 0.0f},
+
+        //     {0.0f,  0.5f,  0.25f, -2.50f, -1.25f,  2.00f,  1.0f, 0.0f},
+        //     {0.0f, -0.5f,  0.25f,  2.50f, -1.25f, -2.00f,  1.0f, 0.0f},
+
+        //     {0.0f,  2.0f,  4.00f, -2.50f, -5.00f,  0.50f,  1.0f, 0.0f},
+        //     {0.0f, -2.0f,  4.00f,  2.50f, -5.00f, -0.50f,  1.0f, 0.0f},
+
+        //     {0.0f, -1.0f,  0.00f,  5.25f,  0.00f, -5.25f,  0.0f, 1.0f}
+        // };
+
         // Bt * d
         for (int j = 0; j < 8; j ++) {
             t_tile[0][j] = tile[0][j] - 5.25f * tile[2][j] + 5.25 * tile[4][j] - tile[6][j];
@@ -737,6 +891,15 @@ int batch_size, int K, int tile_n, int out_map_size, int C) {
                 }
             }   
         }
+
+        // const float At[6][8] = {
+        //     {1.0f,  1.0f,   1.0f,   1.0f,   1.0f,  32.0f, 32.0f, 0.0f},
+        //     {0.0f,  1.0f,  -1.0f,   2.0f,  -2.0f,  16.0f,-16.0f, 0.0f},
+        //     {0.0f,  1.0f,   1.0f,   4.0f,   4.0f,   8.0f,  8.0f, 0.0f},
+        //     {0.0f,  1.0f,  -1.0f,   8.0f,  -8.0f,   4.0f, -4.0f, 0.0f},
+        //     {0.0f,  1.0f,   1.0f,  16.0f,  16.0f,   2.0f,  2.0f, 0.0f},
+        //     {0.0f,  1.0f,  -1.0f,  32.0f, -32.0f,   1.0f, -1.0f, 1.0f}
+        // };
 
         float t_tile[6][8], f_tile[6][6];
         // At * I
@@ -813,4 +976,64 @@ int batch_size, int C, int K, int map_size, int padding) {
     free(trans_input);
     free(trans_filter);
     return;
+}
+
+
+void init(float *A, int size) {
+    for (int i = 0; i < size; i ++) {
+        A[i] = (float)rand() / RAND_MAX;
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    if (argc != 7)
+    {
+        printf("usage: ./test < N > < C > < H(W) > < K > <padding(0/1)> < m(2/4/6) > \n");
+        exit(0);
+    }
+
+    int batch_size = atoi(argv[1]);
+    int C = atoi(argv[2]);
+    int map_size = atoi(argv[3]);
+    int K = atoi(argv[4]);
+    int padding = atoi(argv[5]);
+    int m = atoi(argv[6]);
+    double t_start, t_end;
+
+    float *input = (float *) malloc(batch_size * C * map_size * map_size * sizeof(float));
+    float *weight = (float *) malloc(K * C * 3 * 3 * sizeof(float));
+    float *bias = (float *) malloc(K * sizeof(float));
+    float *result = (float *) malloc(batch_size * K * map_size * map_size * sizeof(float));
+
+    init(input, batch_size * C * map_size * map_size);
+    init(weight, K * C * 3 * 3);
+    init(bias, K);
+
+    t_start = rtclock();
+    switch (m) {
+        case 2:
+            winograd_convolution_2x2(input, weight, bias, result, batch_size, C, K, map_size, padding);
+        break;
+
+        case 4:
+            winograd_convolution_4x4(input, weight, bias, result, batch_size, C, K, map_size, padding);
+        break;
+
+        case 6:
+            winograd_convolution_6x6(input, weight, bias, result, batch_size, C, K, map_size, padding);
+        break;
+
+        default:
+            break;
+    }
+    t_end = rtclock(); 
+    fprintf(stdout, "CPU Runtime: %0.6lfs\n", t_end - t_start); 
+
+    free(input);
+    free(weight);
+    free(bias);
+    free(result);
+    return 0;
 }
